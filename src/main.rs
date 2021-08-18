8ea7e92f0711da35a64de98d18e9239c81a3ea0a
Gleaminggorillas <thomas.henebury@googlemail.com>
2021-08-18 17:27:31 +0100

    all but main

--- src/main.rs
+++ src/main.rs
@@ -44,9 +44,94 @@ struct Particle {
     position: Vec2d<f64>,
     velocity: Vec2d<f64>,
     acceleration: Vec2d<f64>,
-    color: [f32: 4]
+    color: [f32: 4],
 }
 
+impl Particle {
+    fn new(world: &World) -> Particle {
+        let mut rng = thread_rng();
+        let x = rng.gen_range(0.0..=world.width);
+        let y = world.height;
+        let x_velocity = 0.0;
+        let y_velocity = rng.gen_range(-2.0..0.0);
+        let x_acceleration = 0.0;
+        let y_acceleration = rng.gen_range(0.0..0.15);
+
+        Particle {
+            height: 4.0,
+            width: 4.0,
+            position: [x, y].into(),
+            velocity: [x_acceleration,
+                       y_acceleration].into(),
+            color: [1.0, 1.0, 1.0, 0.99],
+        }
+    }
+
+    fn update(&mut self) {
+        self.velocity = add(self.velocity, self.acceleration);
+        self.position = add(self.position, self.velocity);
+        self.acceleration = mul_scalar(self.acceleration, 0.7);
+        self.color[3] *= 0.995;
+    }
+}
+
+impl World {
+    fn new(width: f64, height: f64) -> World {
+        World {
+            current_turn: 0,
+            particles: Vec::<Box<Particle>>::new(),
+            height: height,
+            width: width,
+            rng: thread_rng(),
+        }
+    }
+
+    fn add_shapes(&mut self, n: i32) {
+        for _ in 0..n.abs() {
+            let particle = Particle::new(&self);
+            let boxed_particle = Box::new(particle);
+            self.particles.push(boxed_particle);
+        }
+    }
+
+    fn remove_shapes(&mut self, n: i32) {
+        for _ in 0..n.abs() {
+            let mut to_delete = None;
+            
+            let particle_iter = self.particles.iter().enumerate();
+
+            for (i, particle) in particle_iter {
+                if particle.color[3] < 0.02 {
+                    to_delete = Some(i);
+                }
+                break;
+            }
+
+            if let Some(i) = to_delete {
+                self.particles.remove(i);
+            } else {
+                self.particles.remove(0);
+            };
+        }
+    }
+
+    fn update(&mut self) {
+        let n = self.rng.gen_range(-3..=3);
+
+        if n > 0 {
+            self.add_shapes(n);
+        } else {
+            self.remove_shapes(n);
+        }
+
+        self.particles.shrink_to_fit();
+        for shape in &mut self.particles {
+            shape.update();
+        }
+
+        self.current_turn +=1;
+    }
+}
 
 fn main() {
 }
